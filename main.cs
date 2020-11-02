d2a8e78d15af94f26ffe37e6fbb4d74809e745d1
pKustra <56956247+pKustra@users.noreply.github.com>
2020-11-02 21:03:55 +0100

    Add files via upload

--- main.cs
+++ main.cs
@@ -2,45 +2,71 @@ using System;
  
 public class hangman {
     
+    public hangman() {
+        lives = 5;    
+    }
+    static public int lives;
+    
+    
     static public string Word(){
         string word = "haslo";
         return word;
     }
     
-    static public void guessingChoice(){
+    static public void guessingChoice(string word){
         Console.WriteLine("Would you like to guess letter(l) or whole word(w)?");
         Console.WriteLine("l/w?");
         string choice = Console.ReadLine();
         if (choice == "l"){
-            guessingLetter();
+            guessingLetter(word);
         }
         else if (choice == "w"){
-            guessingWord();
+            guessingWord(word);
         }
         else{
-            guessingChoice();
+            guessingChoice(word);
         }
     }
     
-    static public void guessingLetter(){
-        
-    }
-    
-    static public void guessingWord(){
-        
-    }
-    
-    static public int lives(string word, string guess){
-        int lives = 5;
-        
-        if (word.Contains(guess)){}
-        else {
-            lives--;
-            Console.WriteLine($"lives left {lives}");
+    static public void guessingLetter(string word){
+        Console.WriteLine("Insert letter: ");
+        string letter = Console.ReadLine();
+        if(letter.Length > 1){
+            Console.WriteLine("It should be a letter");
+            guessingLetter(word);
         }
         
-        if (lives == 0) Console.WriteLine("game over");
+        if(word.Contains(letter)){
+            //need to display correct letter
+        }
         
+        else{
+            lives=livesDown(lives, 1);   
+        }
+        
+    }
+    
+    static public void guessingWord(string word){
+        Console.WriteLine("Insert word: ");
+        string gWord = Console.ReadLine();
+        if(gWord == word){
+            Console.WriteLine("You Win!");
+            //end, ask about restart
+        }
+        
+        if(gWord != word){
+            lives=livesDown(lives, 2);
+        }
+    }
+    
+    static public int livesDown(int lives, int howMany){
+        
+        lives=lives-howMany;
+        
+    if (lives < 1) {
+            Console.WriteLine("game over");
+            //tutaj trzeba skonczyc i zapytać o restart
+        }
         return lives;
     }
     
@@ -51,7 +77,6 @@ public class hangman {
 
     static public void Main () {
         display(5);
-        lives("heslo","a");
         return;
     }
 }
